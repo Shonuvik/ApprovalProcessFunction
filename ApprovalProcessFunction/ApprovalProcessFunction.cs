ff393606e4cf6178a63870b59a4c2fec7bf823cc
araujo.marlon@hotmail.com <araujo.marlon@hotmail.com>
2024-01-29 22:08:07 -0300

    First Commit

---
+++ ApprovalProcessFunction/ApprovalProcessFunction.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using ApprovalProcessFunction.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ApprovalProcessFunction
+{
+    public static class ApprovalProcessFunction
+    {
+        [FunctionName("ApprovalProcess_HttpStart")]
+        public static async Task<IActionResult> HttpStart(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+        [DurableClient] IDurableOrchestrationClient starter,
+        ILogger log)
+        {
+            string instanceId = await starter.StartNewAsync("ApprovalProcess", null);
+
+            log.LogInformation($"Iniciada instância de orquestração com ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        [FunctionName("ApprovalProcess")]
+        public static async Task RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context) =>
+            await Approval.ExecuteApprovalProcess(context);
+    }
+}
---
+++ ApprovalProcessFunction/Utils/Approval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace ApprovalProcessFunction.Services
+{
+    public static class Approval
+    {
+        public static async Task ExecuteApprovalProcess(IDurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Etapa 1: Esperar pela aprovação
+            DateTime dueTime = context.CurrentUtcDateTime.AddMinutes(5);
+            Task<bool> approvalTask = context.WaitForExternalEvent<bool>("ApprovalEvent");
+            Task timeoutTask = context.CreateTimer(dueTime, CancellationToken.None);
+
+            // Etapa 2: Aguarda pela conclusão de uma das tarefas.
+            if (approvalTask == await Task.WhenAny(approvalTask, timeoutTask))
+                outputs.Add("Pedido aprovado!");
+            else
+                outputs.Add("O pedido expirou. Aprovação não recebida a tempo.");
+        }
+    }
+}
+
